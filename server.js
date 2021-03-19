db7da7f3bb761285c2434a1a0995aabed4ed0191
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-03-19 18:44:45 +0530

    Ability to add changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/BookStore'));
 // Start the app by listening on the default
 // Heroku port
 app.get('/*', (req, res) =>
 
-    res.sendFile('index.html', { root: 'dist/angular-app-heroku/' }))
+    res.sendFile('index.html', { root: 'dist/BookStore/' }))
 app.listen(process.env.PORT || 8080);
